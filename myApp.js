8e8cce54636b4e6bb16fcade09ad9e6bc9c3f23c
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-07-29 10:49:22 -0500

    Add nodemon to dev dependencies

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function(res, req) {
-  res.send("Hello Express");
+app.get('/', function(req, res) {
+  let path = __dirname + "/views/index.html";
+  res.sendFile(path);
 });
+
+// app.get('/', function(req, res) {
+//   res.send("Hello Express");
+// });
 
 // console.log("Hello World")
 
